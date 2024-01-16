2e58038444d7072660c2fb7eaabd6d495b56ce9b
valera-md <150794469+valera-md@users.noreply.github.com>
2024-01-16 12:02:52 +0200

    Update date_time.py

--- date_time.py
+++ date_time.py
@@ -7,7 +7,7 @@ class Period:
                 self.start = start
                 self.end = end
         except ValueError:
-            print('start date must be less the end date')
+            print('start date must be less then end date')
             exit()
     def  __str__(self):
         return f'"[{self.start} .. {self.end}]"' # e.g. "[01.01.2021 .. 02.01.2021]"
